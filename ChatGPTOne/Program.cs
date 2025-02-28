cfba77c9b060836da1739d413e2dbdd26e797967
ivyycore <mariasequeira.dev@gmail.com>
2025-02-28 20:00:28 +0000

    Finished the exercise by adding the EXIT logic

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -6,38 +6,38 @@ namespace ChatGPTOne
     {
         private static void Main(string[] args)
         {
-            // Variáveis auxiliares
-            string question, response;
-            while (true)
+
+            string question = "";
+            string response;
+            while (question != "EXIT")
             {
-            // Colocar questão ao utilizador
+
                 Console.Write("What's your question? ");
                 question = Console.ReadLine();
 
-            // Verificar se se trata de uma questão conhecida e determinar uma
-            // resposta adequada
+
                     switch (question)
                     {
                         case "How old are you?":
-                            response = "I'm 1 day old!";
+                            Console.WriteLine("I'm 1 day old!");
                             break;
                         case "What do you do?":
-                            response = "Talk to you.";
+                            Console.WriteLine("Talk to you.");
                             break;
                         case "Where are you?":
-                            response = "Inside your computer.";
+                            Console.WriteLine("Inside your computer.");
                             break;
                         case "Can you install a virus in my computer?":
-                            response = "I'm doing it right now!";
+                            Console.WriteLine("I'm doing it right now!");
                             break;
                         case "EXIT":
                             break;
                         default:
-                            response = "I don't think I can answer that";
+                            Console.WriteLine("I don't think I can answer that");
                             break;
+                        
+                    
                     }
-            // Responder adequadamente
-            Console.WriteLine(response);
             }
         }
     }
