dc7911b8bae3c8a8f753cb57581d3a62e73039f1
ivyycore <mariasequeira.dev@gmail.com>
2025-02-28 09:34:05 +0000

    Did the first part (exercicio 6)

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,30 @@
+﻿﻿
+using System;
+
+namespace ValoresEspeciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            sbyte sbMax = sbyte.MaxValue;
+            sbyte sbMin = sbyte.MinValue;
+            byte bMax = byte.MaxValue;
+            byte bMin = byte.MinValue;
+            double dbMin = double.MinValue;
+            double dbMax = double.MaxValue;
+            float fMax = float.MaxValue;
+            float fMin = float.MinValue;
+            ulong ulMax = ulong.MaxValue;
+            ulong ulMin = ulong.MinValue;
+            long lMax = long.MaxValue;
+            long lMin = long.MinValue;
+            int iMax = int.MaxValue;
+            int iMin = int.MinValue;
+            uint uiMax = uint.MaxValue;
+            uint uiMin = uint.MinValue;
+
+            Console.WriteLine($"{sbMax}\n{sbMin}\n{bMax}\n{bMin}\n{dbMax}\n{dbMin}\n{fMax}\n{fMin}\n{ulMax}\n{ulMin}\n{lMax}\n{lMin}\n{iMax}\n{iMin}\n{uiMax}\n{uiMin}");
+        }
+    }
+}
