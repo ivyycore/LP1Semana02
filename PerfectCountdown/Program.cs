f99fab31cf4545c20341c0ee7892ae16c7ea94fc
ivyycore <mariasequeira.dev@gmail.com>
2025-02-28 14:59:00 +0000

    Added the bool values

--- PerfectCountdown/Program.cs
+++ PerfectCountdown/Program.cs
@@ -6,13 +6,18 @@ namespace PerfectCountdown
     {
         private static void Main(string[] args)
         {
-        Console.Write("Start number: ");
-        string question1 = Console.ReadLine();
-        int start = int.Parse(question1);
-        Console.Write("Step number: ");
-        string question2 = Console.ReadLine();
-        int step = int.Parse(question2);
-        }
 
+            Console.Write("Start number: ");
+            string question1 = Console.ReadLine();
+            int start = int.Parse(question1);
+            Console.Write("Step number: ");
+            string question2 = Console.ReadLine();
+            int step = int.Parse(question2);
+
+            bool startb = start > 1;
+            bool stepb = step > start;
+
+
+        }
     }
 }
