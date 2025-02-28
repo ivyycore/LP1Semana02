66e321861efb550e4f4c41c5be8c32f617da883a
ivyycore <mariasequeira.dev@gmail.com>
2025-02-28 02:03:08 +0000

    Did the input command for the script

---
+++ Cilindro/Program.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Altura do cilindro");
+            string a = Console.ReadLine();
+            Console.WriteLine("Raio do cilindro");
+            string r = Console.ReadLine();
+        }
+    }
+}
