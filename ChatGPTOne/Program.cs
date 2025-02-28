c48126062703760faded4516c96f5a2e156da9a1
ivyycore <mariasequeira.dev@gmail.com>
2025-02-28 13:58:45 +0000

    Finished the exercise but to be fixed. Not finished.

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -8,38 +8,37 @@ namespace ChatGPTOne
         {
             // Variáveis auxiliares
             string question, response;
-
+            while (true)
+            {
             // Colocar questão ao utilizador
-            Console.Write("Place your question? ");
-            question = Console.ReadLine();
+                Console.Write("What's your question? ");
+                question = Console.ReadLine();
 
             // Verificar se se trata de uma questão conhecida e determinar uma
             // resposta adequada
-            while (true)
-            {
-                switch (question)
-                {
-                    case "How old are you?":
-                        response = "I'm 1 day old!";
-                        break;
-                    case "What do you do?":
-                        response = "Talk to you.";
-                        break;
-                    case "Where are you?":
-                        response = "Inside your computer.";
-                        break;
-                    case "Can you install a virus in my computer?":
-                        response = "I'm doing it right now!";
-                        break;
-                    case "EXIT"
-                        break;
-                    default:
-                        response = "I don't think I can answer that";
-                        break;
-                }
-            }
+                    switch (question)
+                    {
+                        case "How old are you?":
+                            response = "I'm 1 day old!";
+                            break;
+                        case "What do you do?":
+                            response = "Talk to you.";
+                            break;
+                        case "Where are you?":
+                            response = "Inside your computer.";
+                            break;
+                        case "Can you install a virus in my computer?":
+                            response = "I'm doing it right now!";
+                            break;
+                        case "EXIT":
+                            break;
+                        default:
+                            response = "I don't think I can answer that";
+                            break;
+                    }
             // Responder adequadamente
             Console.WriteLine(response);
+            }
         }
     }
 }
