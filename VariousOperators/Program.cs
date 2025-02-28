df151afbf525776be104385eff46d85617bed1db
ivyycore <mariasequeira.dev@gmail.com>
2025-02-28 01:40:25 +0000

    Did Exercicio 4

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,19 @@
+﻿﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número inteiro-não negativo:");
+            string askNumber = Console.ReadLine();
+            byte saveNumber = byte.Parse(askNumber);
+
+            Console.WriteLine(saveNumber/2);
+            Console.WriteLine(saveNumber<<3);
+            Console.WriteLine(saveNumber^6);
+            Console.WriteLine(saveNumber>10);
+        }
+    }
+}
