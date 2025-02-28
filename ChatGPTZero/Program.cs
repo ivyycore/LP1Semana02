4b65e65abacb96a3e1b16c326b3f5ab3cdb14029
ivyycore <mariasequeira.dev@gmail.com>
2025-02-28 13:09:30 +0000

    Added question to ChatGPTZero

---
+++ ChatGPTZero/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ChatGPTZero
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Variáveis auxiliares
+            string question, response;
+
+            // Colocar questão ao utilizador
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            // Verificar se se trata de uma questão conhecida e determinar uma
+            // resposta adequada
+            switch (question)
+            {
+                case "How are you?":
+                    response = "I'm fine, thank you";
+                    break;
+                case "What's your name?":
+                    response = "Gran'pa";
+                    break;
+                case "What's your mission?":
+                    response = "Destroy mankind!";
+                    break;
+                case "Are you more intelligent than me?":
+                    
+                default:
+                    response = "You got me, I'm not THAT smart!";
+                    break;
+            }
+
+            // Responder adequadamente
+            Console.WriteLine(response);
+        }
+    }
+}
