218b96edb4a8aea328ae659ae8427d3158158b92
ivyycore <mariasequeira.dev@gmail.com>
2025-02-28 13:46:55 +0000

    Added EXIT.

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace ChatGPTZero
+namespace ChatGPTOne
 {
     public class Program
     {
@@ -15,25 +15,29 @@ namespace ChatGPTZero
 
             // Verificar se se trata de uma questão conhecida e determinar uma
             // resposta adequada
-            switch (question)
+            while (true)
             {
-                case "How old are you?":
-                    response = "I'm 1 day old!";
-                    break;
-                case "What do you do?":
-                    response = "Talk to you.";
-                    break;
-                case "Where are you?":
-                    response = "Inside your computer.";
-                    break;
-                case "Can you install a virus in my computer?":
-                    response = "I'm doing it right now!";
-                    break;
-                default:
-                    response = "I don't think I can answer that";
-                    break;
+                switch (question)
+                {
+                    case "How old are you?":
+                        response = "I'm 1 day old!";
+                        break;
+                    case "What do you do?":
+                        response = "Talk to you.";
+                        break;
+                    case "Where are you?":
+                        response = "Inside your computer.";
+                        break;
+                    case "Can you install a virus in my computer?":
+                        response = "I'm doing it right now!";
+                        break;
+                    case "EXIT"
+                        break;
+                    default:
+                        response = "I don't think I can answer that";
+                        break;
+                }
             }
-
             // Responder adequadamente
             Console.WriteLine(response);
         }
