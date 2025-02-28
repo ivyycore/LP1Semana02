17b2d56b24dc70f58365bb5f083a8464bd7d45e1
ivyycore <mariasequeira.dev@gmail.com>
2025-02-28 02:09:55 +0000

    Solved the exercise

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -9,7 +9,19 @@ namespace Cilindro
             Console.WriteLine("Altura do cilindro");
             string a = Console.ReadLine();
             Console.WriteLine("Raio do cilindro");
-            string r = Console.ReadLine();
+            string r = Console.ReadLine();  
+
+            double a1 = double.Parse(a);
+            double r1 = double.Parse(r);
+
+            double V = (Math.PI*(r1*r1)*a1);
+            double S = (2*Math.PI*r1*(r1+a1));
+
+            string resultadoV = $"{V:f3}";
+            string resultadoS = $"{S:f3}";
+            
+            Console.WriteLine($"Volume do cilindro: {resultadoV}");
+            Console.WriteLine($"Área da superfífcie do cilindro: {resultadoS}");
         }
     }
 }
