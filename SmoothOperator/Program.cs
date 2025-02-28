052cf25986bab6811bd6ff1cd977ac85d2dcf9f9
ivyycore <mariasequeira.dev@gmail.com>
2025-02-28 01:04:28 +0000

    Did Exercicio 1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insire um número inteiro:");
+            string askedNumber = Console.ReadLine();
+            sbyte savedNumber = sbyte.Parse(askedNumber);
+            sbyte savedNumber1 = sbyte.Parse(askedNumber);
+            
+            Console.WriteLine(--savedNumber);
+            Console.WriteLine(++savedNumber1);
+            
+        }
+    }
+}
