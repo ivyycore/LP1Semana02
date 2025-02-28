a9ee109954ca1531f0cbb822d33310ff82b390b3
ivyycore <mariasequeira.dev@gmail.com>
2025-02-28 14:30:06 +0000

    Added the variables.

---
+++ PerfectCountdown/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PerfectCountdown
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+        Console.Write("Start number: ");
+        string question1 = Console.ReadLine();
+        int start = int.Parse(question1);
+        Console.Write("Step number: ");
+        string question2 = Console.ReadLine();
+        int step = int.Parse(question2);
+        }
+
+    }
+}
