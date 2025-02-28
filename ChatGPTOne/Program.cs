75ae287d0a98571e322e57c7c705373e3412f2e8
ivyycore <mariasequeira.dev@gmail.com>
2025-02-28 13:39:03 +0000

    Did 1st question & wrong answers.

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 
-namespace ChatGPTOne
+namespace ChatGPTZero
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            // Variáveis auxiliares
+            string question, response;
+
+            // Colocar questão ao utilizador
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            // Verificar se se trata de uma questão conhecida e determinar uma
+            // resposta adequada
+            switch (question)
+            {
+                case "What's your name?":
+                    response = "I'm ChatGPTOne";
+                    break;
+                case "What's your name?":
+                    response = "Gran'pa";
+                    break;
+                case "What's your mission?":
+                    response = "Destroy mankind!";
+                    break;
+                case "Are you more intelligent than me?":
+                    response = "Obviously!";
+                    break;
+                default:
+                    response = "I don't think I can answer that";
+                    break;
+            }
+
+            // Responder adequadamente
+            Console.WriteLine(response);
         }
     }
 }
